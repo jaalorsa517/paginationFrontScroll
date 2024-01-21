aa90fe6bf0665823f66ebc86d494ff9670378848
jaalorsa517 <jaalorsa517@hotmail.com>
2024-01-21 12:29:59 -0500

    Actualizando dependencias

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -2,8 +2,13 @@
 import { ref } from "vue"
 import { J5VIcons } from "@jaalorsa/j5-components-vue"
 import WrapperFull from "./WrapperFull.vue";
+import { langagueES } from "@/js/dictionary";
 
 const isOpen = ref(false);
+
+function translateType(type) {
+  return langagueES[type];
+}
 
 </script>
 <template>
@@ -13,7 +18,11 @@ const isOpen = ref(false);
       <span>Filtrar</span>
     </div>
     <WrapperFull :is-open="isOpen" @close="isOpen = false">
-
+      <ul class="filter__types">
+          <li class="filter__type" v-for="(type, index) in pokemon.info.types" :key="index" :type="type">
+            {{ translateType(type) }}
+          </li>
+        </ul>
     </WrapperFull>
   </div>
 </template>
